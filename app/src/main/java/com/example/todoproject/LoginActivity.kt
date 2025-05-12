2f00b070f23600dda5e338f33f91241026c3d3e0
xyzbk <basharking186@gmail.com>
2025-05-12 12:11:40 +0300

    login finished without mainactivity to be done

--- app/src/main/java/com/example/todoproject/LoginActivity.kt
+++ app/src/main/java/com/example/todoproject/LoginActivity.kt
@@ -1,29 +1,22 @@
 package com.example.todoproject
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import android.widget.Button
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val getStartedBtn = findViewById<Button>(R.id.btnGetStarted)
-        getStartedBtn.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
 
     }
 }
